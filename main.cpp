98f1de41a76242dcdf36bce948ba4d0db69d263b
Keegan <jonesk74@uw.edu>
2018-10-28 16:51:59 -0700

    Add doctest to test the things we write

--- main.cpp
+++ main.cpp
@@ -1,35 +1,10 @@
-#include "sort_algorithms.hpp"
-#include "binary_search_tree.hpp"
-#include "lfu_cache.hpp"
-#include <iostream>
-#include <string>
-#include <cassert>
-#include <array>
-#include <string>
-
-const static std::vector<int> sort_array{ 4, 3, 2, 10, 12, 1, 5, 6 };
-
-template struct std::unique_ptr<bst_node<int>>;
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest.h"
 
 int main(int argc, char* argv[]) {
-    std::cerr << "Begin test.\n";
-
-    auto array_copy = sort_array;
-
-    //selection_sort(array_copy, std::less());
-
-    binary_search_tree<int> tree;
-
-    for (auto val : sort_array) {
-        tree.insert(val);
-    }
-
-    tree.breadth_first_apply([](bst_node<int>* val){ 
-        std::cerr << val->value << " ";
-    });
-
-    lfu_cache<int, std::string, 4> cache;
-    auto retrieved = cache.get(1);
-
-    return 0;
+    doctest::Context ctxt;
+    ctxt.setOption("abort-after", 1);
+    ctxt.applyCommandLine(argc, argv);
+    int result = ctxt.run();
+    return result;
 }
