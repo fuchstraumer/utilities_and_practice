39b5ae4e2d03ee41c86c6f649bf1071aca3b4b09
Keegan <jonesk74@uw.edu>
2018-10-28 17:55:02 -0700

    bubble sort algorithm

--- algorithms/bubble_sort.hpp
+++ algorithms/bubble_sort.hpp
@@ -2,11 +2,18 @@
 #ifndef BUBBLE_SORT_HPP
 #define BUBBLE_SORT_HPP
 #include <functional>
-#include <vector>
 
 template<typename T, typename Comp = std::less<T>>
-void bubble_sort(std::vector<T>& data, Comp cmp) {
+void bubble_sort(T* data, const size_t len, Comp cmp) {
     
+    for (size_t i = 0; i < len; ++i) {
+        for (size_t j = len - 1; j > i; --j) {
+            if (cmp(data[j], data[j - 1])) {
+                std::swap(data[j], data[j - 1]);
+            }
+        }
+    }
+
 }
 
 #endif //!BUBBLE_SORT_HPP
