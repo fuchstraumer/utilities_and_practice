57c2f2a3d359e42cdacdef199e9fff697c14d867
Keegan <jonesk74@uw.edu>
2018-12-23 22:57:11 -0800

    adding quick_sort, WIP radix sort

--- algorithms/sort_algorithms.hpp
+++ algorithms/sort_algorithms.hpp
@@ -4,6 +4,8 @@
 #include <cstddef>
 #include <vector>
 #include <functional>
+#include <limits>
+#include <queue>
 
 template<typename T, typename Func>
 void insertion_sort(std::vector<T>& array, Func&& fn) {
@@ -42,6 +44,54 @@ void bubble_sort(T* data, const size_t len, Comp cmp) {
     }
 }
 
+template<typename T, typename Iter, typename Comp = std::less<T>>
+void quick_sort(Iter begin, Iter end, Comp cmp) {
 
+    auto partition = [&Cmp](Iter lhs, Iter rhs)->Iter {
+        Iter i = lhs - 1;
+        Iter curr = lhs;
+
+        while (curr < rhs) {
+            if (Cmp(*curr, *rhs)) {
+                std::iter_swap(curr, rhs);
+                std::advance(i, 1);
+            }
+            std::advance(i, 1);
+        }
+
+        std::iter_swap((i + 1), rhs);
+        std::advance(i, 1);
+        return i;
+    };
+
+    if (std::distance(begin, end) >= 2) {
+        Iter pivot = partition(begin, end);
+        quick_sort(left, pivot - 1);
+        quick_sort(pivot + 1, right);
+    }
+
+}
+
+template<typename T, typename Iter, typename Comp = std::less<T>>
+void radix_sort(Iter begin, Iter end, Comp cmp) {
+    static_assert(std::is_integral_v<T>, "Value type for radix_sort must be an integral type!");
+    constexpr static size_t DIGITS = std::numeric_limits<T>::digits;
+    constexpr static size_t RADIX = 10u;
+    std::array<std::queue<T>, RADIX> queues;
+
+    for (size_t i = 0, size_t factor = 1u; i < DIGITS; factor *= RADIX, ++i) {
+        for (Iter iter = begin; iter != end; ++iter) {
+            const size_t idx = static_cast<size_t>((*iter / factor) % RADIX);
+            queues[idx].push(*iter);
+        }
+
+        for (size_t j = 0u, Iter iter = begin; j < radix; ++j) {
+            while (!queues[j].empty()) {
+                
+            }
+            ++iter;
+        }
+    }
+}
 
 #endif //!SORT_ALGORITHMS_HPP
